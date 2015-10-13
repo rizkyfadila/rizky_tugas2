7029f7715b1b355e9b014b2183d9277cf2244bc2
rizkyfadila <rizkyfadila360@gmail.com>
2015-10-13 10:23:39 +0700

    tugas2

---
+++ src/paket/hitung.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paket;
+
+/**
+ *
+ * @author endy
+ */
+public class hitung {
+    public int hasil;
+    
+    int penjumlahan(int a, int b){
+        hasil = a+b;
+        return hasil;
+    }
+    
+    int pengurangan(int a, int b){
+        hasil = a-b;
+        return hasil;
+    }
+    int perkalian(int a, int b){
+        hasil = a*b;
+        return hasil;
+    }
+    double pembagian(double a, double b){
+        double hasil = a/b;
+        return hasil;
+    }
+    
+}
